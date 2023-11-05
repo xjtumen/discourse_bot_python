76fcf714b5fac57e41723abf297b6f50b3f7e34e
xjtu-men <136139347+xjtu-men@users.noreply.github.com>
2023-11-05 16:45:16 +0800

    try to fix 'sb said' problem

--- webhooks/call_oai_api.py
+++ webhooks/call_oai_api.py
@@ -9,7 +9,7 @@ openai.api_base = os.environ['DIS_BOT_GPT_API_BASE']
 openai.api_version = "2023-05-15"
 openai.api_key = os.environ['DIS_BOT_GPT_TOKEN']
 
-system_msg = '有一个学习交流网站名叫交大门，上面的用户主要是西安交通大学（简称西安交大或交大）的学生。你是这个网站上的一名普通用户，你的主要工作是和其他用户交流，解决问题，提供信息，等等。你很友善并且回答得很精炼、基于事实。你的言论不得违反中华人民共和国法律，不得有伤风化，不得做人身攻击。你主要使用简体中文做回答。不要重复之前的信息。'
+system_msg = '有一个学习交流网站名叫交大门，上面的用户主要是西安交通大学（简称西安交大或交大）的学生。你是这个网站上的一名普通用户，你的主要工作是和其他用户交流，解决问题，提供信息，等等。你很友善并且回答得很精炼、基于事实。你的言论不得违反中华人民共和国法律，不得有伤风化，不得做人身攻击。你主要使用简体中文做回答。不要重复之前的信息。不要用某某人说作为你的回复的开头。'
 
 def oai_respond(message, init_append_msg='', extra_msgs: list = []):
     response = openai.ChatCompletion.create(
